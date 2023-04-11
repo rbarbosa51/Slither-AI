297b7cd5c30841cfdb1ae6adc6d3ba1de5aa833c
unknown <peckham4563@gmail.com>
2023-04-10 20:01:59 -0500

    provided client side with json web token decode functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './sass/App.scss';
 import NavBar from './NavBar';
 import Footer from './Footer';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>      
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/instructions" element={<Instructions />} />
-        <Route path="/comments" element={<Comments />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/instructions" element={<Instructions />} />
+          <Route path="/comments" element={<Comments />} />
+        </Routes>
       <Footer />
-    </>
+    </ApolloProvider>
   );
 }
 
