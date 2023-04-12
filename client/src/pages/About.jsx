2f4bcb6b2c3badaa7b801791c241b4062329516b
amymckellar <amy_mckellar@yahoo.com>
2023-04-11 19:05:13 -0500

    added react component for about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,11 +5,32 @@ import "../sass/About.scss";
 
 export default function About() {
   return (
-    <div className="about">
-      <h1>About</h1>
+    <div className="About">
+      <h1>About Us!</h1>
       <p>
-        This is a simple example of a React app with a Node.js backend.
+        We are so excited to share this project with you. Get to know us!
+      </p>
+      <h1>Amy</h1>
+      <p>
+      Hey hey! My name is Amy McKellar and I am a Natural Resource Specialist at Texas Commission on Environmental Quality. I love everything outdoors and spending time in nature. I enjoy learning new things and challenging myself.
+      </p>
+      <h1>Alexander</h1>
+      <p>
+      My name is Alexander Peckham and I live in Houston, Texas. I enjoy sports, mechanics, and spending time with my dogs. I have been a student at the University of Houston since 2018 in pursuit of a bachelor’s degree in engineering. However, I decided to take a break to enroll in the Full-Stack Coding Boot Camp through UT Austin. I hope to be able to obtain a job after the bootcamp to help me pursue my interests.
+      </p>
+      <h1>Rafael</h1>
+      <p>
+      I am a veteran, who has served in both Afghanistan and Iraq. While in the Army, I served as a sys admin, and as a military Intelligence Analyst. I wrote a Book on Blender Geometry nodes that currently sells on Amazon. I am seeking to improve myself and my living conditions by attending the coding bootcamp.
+      </p>
+      <h1>Matthew</h1>
+      <p>
+      My name is Matthew Walker and I live in Austin, Texas. I am 19 years old, and I enjoy working out, coding, and playing basketball. My plan is to go to UT and find a job in software hopefully, Apple later down the line. I enjoy both art and coding, so web development is a good way to express art through the design feature, which is the reason I chose this course. I plan to learn a lot more languages after this course such as Python, the C languages, and possibly swift for Apple. Thank you, gg.
+      </p>
+      <h1>Rojan</h1>
+      <p>
+      My name is Rojan Gurbuz and I am a relatively young coder who is very passionate about the creativity involved in coding. I have a lot of fun working together with others on projects and reaching a shared goal.
       </p>
     </div>
+    
   );
 }
