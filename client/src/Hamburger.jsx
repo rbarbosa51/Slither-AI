ad05e913e339507e16245c274a354ff9e1bb1b91
gulin225 <gurbuzroj@gmail.com>
2023-04-13 22:00:25 -0500

    created hamburger menu dropdown for navbar at smaller screen sizes for mobile UI accesibility

---
+++ client/src/Hamburger.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './sass/Hamburger.scss';
+
+export default function Hamburger () {
+    return (
+        <>
+            <div className="hamburger">
+                <div className="burger burger1" />
+                <div className="burger burger2" />
+                <div className="burger burger3" />
+            </div>
+        </>
+    )
+}
